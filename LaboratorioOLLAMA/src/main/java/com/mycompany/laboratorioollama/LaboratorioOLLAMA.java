16cfa18e1d06d32a80da97d22ec65b8ec42f42ec
DeiberH <deiberh@uninorte.edu.co>
2024-11-08 14:03:25 -0500

    cambio de proyecto
    se coloco el archivo correcto dentro

---
+++ LaboratorioOLLAMA/src/main/java/com/mycompany/laboratorioollama/LaboratorioOLLAMA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.laboratorioollama;
+
+
+
+/**
+ *
+ * @author Derby42
+ */
+public class LaboratorioOLLAMA {
+
+    public static void main(String[] args) {
+        UIOllama frame = new UIOllama();
+        frame.setVisible(true);
+        frame.setSize(865, 558);
+    }
+}
